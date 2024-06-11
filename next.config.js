9f95bedf46f70cbd163fb036c31e13d54dcfb906
RGarciaZGames <68579638+RichardGarciaP@users.noreply.github.com>
2024-06-11 14:30:39 -0500

    fix: add dependencies

--- next.config.js
+++ next.config.js
@@ -1,15 +1,37 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
 const nextConfig = {
   env: {
-    // un comment when you run in deploy
-    // API_URL: "https://cuba-nextjs.vercel.app/api",
-    //  comment when you run in local below down
-    API_URL: "http://54.92.233.245:3005/v1/api",
+    // Comentarios para distinguir entre entornos
+    // API_URL: "https://cuba-nextjs.vercel.app/api", // Usar en producción
+    API_URL: "http://54.92.233.245:3005/v1/api", // Usar en desarrollo local
   },
   redirects: async () => {
     return [
       { source: "/", destination: "/authentication/login", permanent: true },
     ];
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: process.env.API_URL + '/:path*'
+      }
+    ]
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.devServer = config.devServer || {};
+      config.devServer.proxy = {
+        '/api': {
+          target: process.env.API_URL,
+          changeOrigin: true,
+          pathRewrite: { '^/api': '' }
+        }
+      };
+    }
+    return config;
+  }
 };
 
 module.exports = nextConfig;
